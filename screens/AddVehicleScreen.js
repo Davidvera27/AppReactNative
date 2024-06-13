7561c29d23be8494618818a385961f9a4597115d
Davidvera27 <davidrestrepovera@gmail.com>
2024-06-13 17:06:21 -0500

    Update AddVehicleScreen.js

--- screens/AddVehicleScreen.js
+++ screens/AddVehicleScreen.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Image, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
 import uuid from 'react-native-uuid';
-import defaultCarIcon from '../assets/default_car_icon.png'; // Ruta corregida
+import defaultCarIcon from '../assets/default_car_icon.png';
 
 const AddVehicleScreen = ({ route }) => {
   const { addVehicle } = route.params;
@@ -39,58 +39,103 @@ const AddVehicleScreen = ({ route }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Marca:</Text>
-      <TextInput
-        style={styles.input}
-        value={brand}
-        onChangeText={setBrand}
-        placeholder="Ingrese la marca"
-      />
-      <Text style={styles.label}>Modelo:</Text>
-      <TextInput
-        style={styles.input}
-        value={model}
-        onChangeText={setModel}
-        placeholder="Ingrese el modelo"
-      />
-      <Text style={styles.label}>Placa:</Text>
-      <TextInput
-        style={styles.input}
-        value={plate}
-        onChangeText={setPlate}
-        placeholder="Ingrese la placa"
-      />
-      <Text style={styles.label}>Logo:</Text>
-      <Image source={logo} style={styles.logo} />
-      <Button title="Seleccionar Imagen" onPress={pickImage} />
-      <Button title="Guardar Vehículo" onPress={handleSave} />
-    </View>
+    <ImageBackground source={require('../assets/BackGround_HOME_6.png')} style={styles.backgroundImage} imageStyle={styles.backgroundImageStyle}>
+      <View style={styles.overlay}>
+        <View style={styles.container}>
+          <Text style={styles.label}>Marca:</Text>
+          <TextInput
+            style={styles.input}
+            value={brand}
+            onChangeText={setBrand}
+            placeholder="Ingrese la marca"
+            placeholderTextColor="#072949"
+          />
+          <Text style={styles.label}>Modelo:</Text>
+          <TextInput
+            style={styles.input}
+            value={model}
+            onChangeText={setModel}
+            placeholder="Ingrese el modelo"
+            placeholderTextColor="#072949"
+          />
+          <Text style={styles.label}>Placa:</Text>
+          <TextInput
+            style={styles.input}
+            value={plate}
+            onChangeText={setPlate}
+            placeholder="Ingrese la placa"
+            placeholderTextColor="#072949"
+          />
+          <Text style={styles.label}>Logo:</Text>
+          <TouchableOpacity onPress={pickImage}>
+            <Image source={logo} style={styles.logo} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
+            <Text style={styles.saveButtonText}>Guardar Vehículo</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backgroundImageStyle: {
+    resizeMode: 'cover', // Ensure the image covers the whole background
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.7)', // Dark overlay to enhance readability
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    width: '90%',
+    backgroundColor: 'rgba(255, 255, 255, 0.9)', // Slightly transparent white background
     padding: 20,
-    backgroundColor: '#fff',
+    borderRadius: 20,
   },
   label: {
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
+    color: '#000',
   },
   input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
+    borderWidth: 3,
+    borderColor: '#115da3',
     padding: 10,
     fontSize: 18,
     marginBottom: 20,
+    borderRadius: 10,
+    color: '#000',
   },
+
   logo: {
     width: 100,
     height: 100,
     marginBottom: 20,
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
+  saveButton: {
+    backgroundColor: '#115da3',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  saveButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 18,
   },
 });
 
