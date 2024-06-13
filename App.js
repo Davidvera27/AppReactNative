22c6c56a1b9b6220c4a8f42bddb4abbab54dceca
Davidvera27 <davidrestrepovera@gmail.com>
2024-06-13 17:05:50 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
-
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 import RemindersListScreen from './screens/RemindersListScreen';
@@ -9,21 +8,27 @@ import AddVehicleScreen from './screens/AddVehicleScreen';
 
 const Stack = createStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
         screenOptions={{
-          headerShown: true,
-          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          headerStyle: {
+            backgroundColor: '#115da3', // Cambiar color de la barra
+          },
+          headerTintColor: '#fff', // Color del texto del encabezado
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
         }}
       >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="RemindersList" component={RemindersListScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'HOME' }} />
+        <Stack.Screen name="Details" component={DetailsScreen} options={{ title: 'Details' }} />
+        <Stack.Screen name="RemindersList" component={RemindersListScreen} options={{ title: 'RemindersList' }} />
         <Stack.Screen name="AddVehicle" component={AddVehicleScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
